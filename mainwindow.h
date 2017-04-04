520bbc1a3e9bb0a460b6ffb727b53d1699955386
Zeporath <shekhar.jaigaon@gmail.com>
2017-04-05 02:43:29 +0530

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -20,12 +20,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void getPorts();
+    void getBaudRate();
+
 private slots:
     void readSerial();
     void updateLCDs(QString);
+    void on_start_push_button_clicked();
+    void logger_mgr(QString);
+    void on_logger_checkBox_stateChanged(int arg1);
+
 private:
     Ui::MainWindow *ui;
 
+    bool startlogging;
     static const quint16 arduino_uno_vendor_id= 9025;
     static const quint16 arduino_uno_product_id = 67;
     QSerialPort *arduino;
