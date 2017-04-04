84d2a772abf1be44978dcc2abc8b2a1ac27339d3
Zeporath <shekhar.jaigaon@gmail.com>
2017-04-04 19:20:44 +0530

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QMessageBox>
+#include <QDebug>
+#include <string>
+
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +20,21 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+private slots:
+    void readSerial();
+    void updateLCDs(QString);
 private:
     Ui::MainWindow *ui;
+
+    static const quint16 arduino_uno_vendor_id= 9025;
+    static const quint16 arduino_uno_product_id = 67;
+    QSerialPort *arduino;
+    QByteArray serialData;
+    QString serialBuffer;
+    QString parsed_data;
+
+
+
 };
 
 #endif // MAINWINDOW_H
