35465c981b1a404189934e17525d2d14a0dbd1e0
Zeporath <shekhar.jaigaon@gmail.com>
2017-04-25 14:27:14 +0530

    Update control_box.h

--- control_box.h
+++ control_box.h
@@ -1,22 +1,61 @@
-#ifndef CONTROL_BOX_H
-#define CONTROL_BOX_H
+#ifndef CONTOL_WINDOW_H
+#define CONTOL_WINDOW_H
 
-#include <QDialog>
+#include <QMainWindow>
+#include <QString>
+#include <QPushButton>
 
 namespace Ui {
-class Control_BOX;
+class Contol_window;
 }
 
-class Control_BOX : public QDialog
+class Contol_window : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit Control_BOX(QWidget *parent = 0);
-    ~Control_BOX();
+    explicit Contol_window(QWidget *parent = 0);
+    ~Contol_window();
+
+
+private slots:
+    void on_OV_Button_clicked();
+    void initControls();
+
+
+    void on_CC_READY_pushButton_clicked();
+
+    void on_OCD_pushButton_clicked();
+
+    void on_LOAD_PRESENT_pushButton_clicked();
+
+    void on_UV_pushButton_clicked();
+
+    void on_SCD_pushButton_clicked();
+
+    void on_ADC_EN_pushButton_clicked();
+
+    void on_DSG_ON_pushButton_clicked();
+
+    void on_CHG_ON_pushButton_clicked();
+
+    void on_DEVICE_X_READY_pushButton_clicked();
+
+    void on_TEMP_SEL_pushButton_clicked();
+
+    void on_CC_pushButton_clicked();
+
+    void on_DELAY_DIS_pushButton_clicked();
+
+    void on_OVERD_ALERT_pushButton_clicked();
+
+    void on_RSNS_pushButton_clicked();
 
 private:
-    Ui::Control_BOX *ui;
-};
+    Ui::Contol_window *ui;
+    QString button_color="green";
+//    void initgui();
+    int changeColor(QPushButton* button, QString* button_color1);
 
-#endif // CONTROL_BOX_H
+
+#endif // CONTOL_WINDOW_H
